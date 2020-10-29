c03be2b586fd8831694b62e9fafc25809d7fddda
Kenneth Lu <ken.zc.lu@gmail.com>
2020-10-30 02:12:50 +1100

    Basic setup for About

--- src/components/pages/about/about.js
+++ src/components/pages/about/about.js
@@ -1,28 +1,54 @@
 import React from "react";
-import AboutMarkdown from "../../../assets/about.md";
-import ReactMarkdown from "react-markdown";
+
+import Icon from "../../atoms/icon/Icon";
+import linkedin from "../../../assets/linkedin.png";
+import document from "../../../assets/document.png";
+import github from "../../../assets/github.png";
 import "./about.scss";
 
-export default class About extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      text: null,
-    };
-  }
-
-  async componentWillMount() {
-    const fetched = await fetch(AboutMarkdown);
-    const text = await fetched.text();
-    this.setState({ text: text });
-  }
-
-  render() {
-    return (
-      <div className="about">
-        <ReactMarkdown source={this.state.text} />
+function About() {
+  return (
+    <div className="about">
+      <h1>Hi,</h1>
+      <div>
+        My name is Kenneth. Welcome to my portfolio built using <i>ReactJS</i>,
+        you can view the source code{" "}
+        <a
+          href="https://github.com/tarotiger/homepage"
+          style={{ textDecoration: "underline" }}
+        >
+          here
+        </a>
+        .
       </div>
-    );
-  }
+      <div>
+        I'm currently completing my{" "}
+        <b>Bachelors of Science in Computer Science</b> at the{" "}
+        <b>University of New South Wales</b>. I'm also working as a part-time
+        software developer at Excelerate Consulting, a consulting firm which
+        sells banking education solutions.
+      </div>
+      <div>
+        Apart from coding and working, I like to relax by playing video games
+        and board games. Unfortunately, due to COVID-19, have not had many
+        opportunities to do the latter.
+      </div>
+      <div className="container">
+        <Icon
+          src={linkedin}
+          href="https://www.linkedin.com/in/kenneth-zc-lu/"
+          label="LinkedIn"
+        />
+        <Icon
+          src={document}
+          href=""
+          download="resume_kenneth_l"
+          label="Resume"
+        />
+        <Icon src={github} href="https://github.com/tarotiger" label="Github" />
+      </div>
+    </div>
+  );
 }
+
+export default About;
--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
-import React from "react"
-import { BrowserRouter, Redirect, Switch, Route } from "react-router-dom"
+import React from "react";
+import { BrowserRouter, Redirect, Switch, Route } from "react-router-dom";
 
-import Navbar from "./components/molecules/navbar/navbar"
-import About from "./components/pages/about/about"
-import Project from "./components/project"
-import Contact from "./components/contact"
+import Navbar from "./components/molecules/navbar/navbar";
+import About from "./components/pages/about/about";
+import Project from "./components/project";
+import Contact from "./components/contact";
 
 export const paths = [
   {
@@ -29,19 +29,21 @@ function Base() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Switch>
-        {paths.map((path, i) => {
-          return (
-            <Route
-              key={`path-${i}`}
-              exact={path.exact || false}
-              path={path.path}
-              component={path.component}
-            />
-          );
-        })}
-        <Redirect to="/" />
-      </Switch>
+      <div className="page-container">
+        <Switch>
+          {paths.map((path, i) => {
+            return (
+              <Route
+                key={`path-${i}`}
+                exact={path.exact || false}
+                path={path.path}
+                component={path.component}
+              />
+            );
+          })}
+          <Redirect to="/" />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
